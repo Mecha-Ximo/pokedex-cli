283958cc59aec2274c2742fe2a9b850e6368526a
Ximo <ximo.dev.94@gmail.com>
2025-01-16 20:43:50 +0100

    extract code into function

--- main.go
+++ main.go
@@ -1,27 +1,6 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-  scanner := bufio.NewScanner(os.Stdin)
-
-  for {
-    fmt.Print(prompt)
-
-    scanner.Scan()
-    rawCmd := scanner.Text()
-    cmdWords := cleanInput(rawCmd)
-
-    if len(cmdWords) == 0 {
-      continue
-    }
-
-    fmt.Printf("Your command was: %s\n", cmdWords[0])
-  }
-
+  startRepl()
 }
 
--- repl.go
+++ repl.go
@@ -1,9 +1,32 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+	  fmt.Print(prompt)
+  
+	  scanner.Scan()
+	  rawCmd := scanner.Text()
+	  cmdWords := cleanInput(rawCmd)
+  
+	  if len(cmdWords) == 0 {
+		continue
+	  }
+  
+	  fmt.Printf("Your command was: %s\n", cmdWords[0])
+	}
+}
 
 func cleanInput(text string) []string {
 	words := strings.Fields(strings.ToLower(text))
   
 	return words
-  }
+}
